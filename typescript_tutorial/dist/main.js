54b84420783c170eba947c4e8332db1aacb786b6
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-01-23 21:31:12 +0200

    small changes

--- typescript_tutorial/dist/main.js
+++ typescript_tutorial/dist/main.js
@@ -1,2 +1,24 @@
+// Variables
 var a = '1';
 console.log("aaa", a);
+var hello = "world";
+// Functions
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName('Moster', 'Lessons'));
+var user = {
+    name: 'Monster',
+    age: 30,
+    getMessage: function () {
+        return 'hello' + name;
+    },
+};
+/*const user2: User = {
+    name: "Phil"
+}*/
+/*const user: {name: string; age: number} = {
+    name: 'Nyakallo',
+    age: 11,
+};*/
+// Types and Unions
--- typescript_tutorial/src/main.ts
+++ typescript_tutorial/src/main.ts
@@ -1,3 +1,44 @@
+// Variables
+
 const a = '1';
 
 console.log("aaa", a)
+
+let hello: string = "world";
+
+// Functions
+
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname;
+};
+
+console.log(getFullName('Moster', 'Lessons'))
+
+// Interfaces
+
+interface UserInterface {
+    name: string;
+    age?: number;
+    getMessage(): string
+}
+
+const user: UserInterface = {
+    name: 'Monster',
+    age: 30,
+    getMessage() {
+        return 'hello' + name;
+    },
+};
+
+/*const user2: User = {
+    name: "Phil"
+}*/
+
+
+/*const user: {name: string; age: number} = {
+    name: 'Nyakallo',
+    age: 11,
+};*/
+
+// Types and Unions
+
