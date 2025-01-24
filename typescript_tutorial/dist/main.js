81e53e5c912dff1c3e777938638ef80236f08a17
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-01-24 23:16:07 +0200

    small changes

--- typescript_tutorial/dist/main.js
+++ typescript_tutorial/dist/main.js
@@ -1,12 +1,23 @@
 // Variables
-var a = '1';
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var a = "1";
 console.log("aaa", a);
 var hello = "world";
 // Functions
 var getFullName = function (name, surname) {
     return name + " " + surname;
 };
-console.log(getFullName('Moster', 'Lessons'));
+console.log(getFullName("Moster", "Lessons"));
 // Interfaces
 /*interface UserInterface {
     name: string;
@@ -33,31 +44,44 @@ const user: {name: string; age: number} = {
 };*/
 // Types and Unions
 // Union Operator
-var username = 'alex';
-var pageName = '1';
+var username = "alex";
+var pageName = "1";
 var errorMessage = null;
-var PopularTags = ['dragon', 'coffe'];
-var dragonsTag = 'dragon';
+//let user3: UserInterface | null = null;
+//Type Alliases in typescript
+/*type ID = string;
+type PopularTag = string;
+
+interface UserInterface {
+  id: ID;
+  name: string;
+  surname: string;
+}
+
+const PopularTags: PopularTag[] = ["dragon", "coffe"];*/
+//Unions + type alliases
+//type MaybePopularTag = PopularTag | null;
+//const dragonsTag: MaybePopularTag = "dragon";
 //Any/Void/Never/Unknown
 //Void
 var doSomething = function () {
-    console.log('doSomething');
+    console.log("doSomething");
 };
 //Any
-var foo = 'foo';
+var foo = "foo";
 console.log(foo.bar());
 //Never
 var playSomething = function () {
-    throw 'never';
+    throw "never";
 };
 //Unknown
 var vAny = 10;
 var vUnknown = 10;
 var s1 = vAny;
 //let s2: string = vUnknown;
-//Type Assertion 
+//Type Assertion
 var s2 = vUnknown;
-var pageNumber = '1';
+var pageNumber = "1";
 var numericPageNumber = pageNumber;
 //Working with the Dom
 //working with input data
@@ -65,4 +89,44 @@ var numericPageNumber = pageNumber;
 
 console.log('someElement', someElement.value);*/
 //adding listerners
-var someElement = document.querySelector('.foo');
+var someElement = document.querySelector(".foo");
+someElement.addEventListener("blur", function (event) {
+    var target = event.target;
+    console.log("event", target.value);
+});
+//creating classes
+/*class User {
+    protected firstName: string;
+    private lastName: string;
+    readonly unchangeableName: string
+
+   constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullname(): string {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const user = new User("Monster", "lessons");*/
+//Generic interfaces and functions
+var addId = function (obj) {
+    //the T is Generic which is used as a data type
+    var id = Math.random().toString(16);
+    return __assign(__assign({}, obj), { id: id });
+};
+var user = {
+    name: "jack",
+    data: {
+        meta: 'foo',
+    }
+};
+var user2 = {
+    name: 'John',
+    data: ['foo', 'bar', 'baz']
+};
+var result = addId(user);
+console.log("result", result);
+//Enums
--- typescript_tutorial/src/main.ts
+++ typescript_tutorial/src/main.ts
@@ -1,22 +1,20 @@
 // Variables
 
-const a = '1';
+const a = "1";
 
-console.log("aaa", a)
+console.log("aaa", a);
 
 let hello: string = "world";
 
 // Functions
 
 const getFullName = (name: string, surname: string): string => {
-    return name + " " + surname;
+  return name + " " + surname;
 };
 
-console.log(getFullName('Moster', 'Lessons'))
+console.log(getFullName("Moster", "Lessons"));
 
 // Interfaces
-
-
 
 /*interface UserInterface {
     name: string;
@@ -46,52 +44,50 @@ const user: {name: string; age: number} = {
 
 // Union Operator
 
+let username: string = "alex";
 
-
-let username: string = 'alex';
-
-let pageName: string | number = '1';
+let pageName: string | number = "1";
 
 let errorMessage: string | null = null;
 
 //let user3: UserInterface | null = null;
 
- //Type Alliases in typescript
+//Type Alliases in typescript
 
-type ID = string;
+/*type ID = string;
 type PopularTag = string;
 
 interface UserInterface {
-    id: ID;
-    name: string;
-    surname: string;
+  id: ID;
+  name: string;
+  surname: string;
 }
 
-const PopularTags: PopularTag[] = ['dragon', 'coffe']
+const PopularTags: PopularTag[] = ["dragon", "coffe"];*/
 
 //Unions + type alliases
 
-type MaybePopularTag = PopularTag | null
+//type MaybePopularTag = PopularTag | null;
 
-const dragonsTag: MaybePopularTag = 'dragon';
+//const dragonsTag: MaybePopularTag = "dragon";
 
 //Any/Void/Never/Unknown
 
 //Void
 
 const doSomething = (): void => {
-    console.log('doSomething');
+  console.log("doSomething");
 };
 
 //Any
 
-let foo: any = 'foo';
+let foo: any = "foo";
 console.log(foo.bar());
 
 //Never
 
 const playSomething = (): never => {
-    throw 'never';
+  throw "never";
 };
 
 //Unknown
@@ -102,12 +98,12 @@ let vUnknown: unknown = 10;
 let s1: string = vAny;
 //let s2: string = vUnknown;
 
-//Type Assertion 
+//Type Assertion
 
-let s2: string =vUnknown as string;
+let s2: string = vUnknown as string;
 
-let pageNumber: String = '1';
-let numericPageNumber: number = (pageNumber as unknown) as number;
+let pageNumber: String = "1";
+let numericPageNumber: number = pageNumber as unknown as number;
 
 //Working with the Dom
 //working with input data
@@ -117,5 +113,61 @@ console.log('someElement', someElement.value);*/
 
 //adding listerners
 
-const someElement = document.querySelector('.foo');
+const someElement = document.querySelector(".foo");
 
+someElement.addEventListener("blur", (event) => {
+  const target = event.target as HTMLInputElement;
+  console.log("event", target.value);
+});
+
+//creating classes
+
+/*class User {
+    protected firstName: string;
+    private lastName: string;
+    readonly unchangeableName: string
+
+   constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    getFullname(): string {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const user = new User("Monster", "lessons");*/
+
+//Generic interfaces and functions
+
+const addId = <T extends object>(obj: T) => {
+  //the T is Generic which is used as a data type
+  const id = Math.random().toString(16);
+  return {
+    ...obj,
+    id,
+  };
+};
+
+interface UserInterface<T> {
+  name: string;
+  data: T;
+}
+
+const user: UserInterface<{meta: string}> = {
+  name: "jack",
+  data: {
+    meta: 'foo',
+  }
+};
+
+const user2: UserInterface<string[]> = {
+    name: 'John',
+    data: ['foo', 'bar', 'baz']
+}
+
+const result = addId<UserInterface>(user);
+console.log("result", result);
+
+//Enums
