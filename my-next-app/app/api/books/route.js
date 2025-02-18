da8fee72c4216a9c6ba79114c16a5bb2dec7e7d5
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-02-18 10:41:50 +0200

    small changes

--- my-next-app/app/api/books/route.js
+++ my-next-app/app/api/books/route.js
@@ -1,13 +1,16 @@
+
 import { prisma } from "../../db";
-import books from "./data.json";
+//import books from "./data.json";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
+
+
 export async function GET(req) {
+  
   const books = await prisma.book.findMany();
   console.log("GET books called");
-
-  return NextResponse.json(quotes);
+  return NextResponse.json(books);
 }
 
 export async function POST(req) {
