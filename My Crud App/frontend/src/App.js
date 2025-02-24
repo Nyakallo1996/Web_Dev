a908021c5ba564c77f07934043f1ba007ea0821e
Nyakallo Mahlakametsa <142156744+Nyakallo1996@users.noreply.github.com>
2025-02-24 10:18:53 +0200

    small changes

--- My Crud App/frontend/src/App.js
+++ My Crud App/frontend/src/App.js
@@ -1,4 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,6 +11,7 @@ function App() {
     <>
       <Router>
         <div className="container">
+          
           <Routes>
             <Route path="/" element={<Dashboard/>} />
             <Route path="/login" element={<Login/>} />
@@ -15,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer/>
     </>
   );
 }
